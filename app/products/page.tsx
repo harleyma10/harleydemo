05cb584e3e93934f15396eeabdecdd4323915afa
harleyma10 <69475546+harleyma10@users.noreply.github.com>
2026-04-01 22:51:28 -0700

    Create products management page

---
+++ app/products/page.tsx
@@ -0,0 +1,14 @@
+// This is the products management page for the application.
+
+import React from 'react';
+
+const ProductsPage = () => {
+    return (
+        <div>
+            <h1>Products Management</h1>
+            {/* Add your product management functionalities here */}
+        </div>
+    );
+};
+
+export default ProductsPage;
